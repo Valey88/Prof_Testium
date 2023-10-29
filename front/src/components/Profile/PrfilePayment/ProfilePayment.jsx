fcc5d73d0345fd8e3eddacfa083218306103b24c
valej <Valej200554@gmail.com>
2023-10-29 16:01:32 +0500

    add

--- front/src/components/Profile/PrfilePayment/ProfilePayment.jsx
+++ front/src/components/Profile/PrfilePayment/ProfilePayment.jsx
@@ -1,27 +1,40 @@
 import style from "./ProfilePayment.module.css";
+import Button from "../../globalComponents/Buttons/Button";
 function ProfilePayment() {
   return (
     <div className={style.ProfilePaymentBlock}>
       <div className={style.ProfilePaymentHeading}>
-        <h2>Ваши карты</h2>
+        <h3>Ваши карты</h3>
         <div className={style.ProfilePaymentBlockInside}>
           <div className={style.PaymentBlock}>
-            <img src="" alt="" />
-            <p>xxxx xxxx xxxx 3829</p>
-            <button>
-              <img src="" alt="" />
-            </button>
-            <button></button>
+            <div>
+              <img src="/src/assets/icon/free-icon-visa-349221.png" alt="" />
+              <p>xxxx xxxx xxxx 3829</p>
+            </div>
+            <div>
+              <button>
+                <img src="/src/assets/icon/piper.svg" alt="" />
+              </button>
+              <button>
+                <img src="/src/assets/icon/rabit.svg" alt="" />
+              </button>
+            </div>
           </div>
         </div>
         <div className={style.AddPaymentBlock}>
           <div>
-            <button>
-              <div>
-                <p></p>
-                <img src="" alt="" />
+            <Button backBtn="#f5f5f5" widthBtn={250}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <img src="/src/assets/icon/plus.svg" alt="" />
+                <p>Добавить карту</p>
               </div>
-            </button>
+            </Button>
           </div>
         </div>
       </div>
--- front/src/pages/Profile/Profile.jsx
+++ front/src/pages/Profile/Profile.jsx
@@ -1,22 +1,22 @@
-
-import ProfileHeader from '../../components/Profile/ProfileHeader/ProfileHeader'
-import ProfileInfo from '../../components/Profile/ProfileInfo/ProfileInfo'
-import style from './Profile.module.css'
+import ProfilePayment from "../../components/Profile/PrfilePayment/ProfilePayment";
+import ProfileHeader from "../../components/Profile/ProfileHeader/ProfileHeader";
+import ProfileInfo from "../../components/Profile/ProfileInfo/ProfileInfo";
+import style from "./Profile.module.css";
 
 const Profile = () => {
-    return (
-        <>
-            <div className={style.Profile}>
-                <div className={style.Role}>Role</div>
-                <h1>Мой профиль</h1>
-                <div className={style.ProfileBlock}>
-                    <ProfileHeader />
-                    <ProfileInfo />
-                </div>
-            </div>
-        </>
-    )
-}
-
+  return (
+    <>
+      <div className={style.Profile}>
+        <div className={style.Role}>Role</div>
+        <h1>Мой профиль</h1>
+        <div className={style.ProfileBlock}>
+          <ProfileHeader />
+          {/* <ProfileInfo /> */}
+          <ProfilePayment />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
